ff3e742bebf91ffcf80f41a08241caa150d4c46d
root <root@debian>
2013-10-12 13:26:50 +0200

    snd push

--- main.hs
+++ main.hs
@@ -2,3 +2,8 @@ main :: IO ()
 main = do putStrLn "Hello!"
 	  putStrLn "First commit!"
 	  putStrLn "What to come?!"
+          morePrinting
+
+morePrinting :: IO ()
+morePrinting = do putStrLn "Some testing"
+  	    	  putStrLn "Will this work?"
