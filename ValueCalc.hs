aad668edb782f532ddd02d63fbcd060859c0cb92
kristian@purestyle.se <kristian@purestyle.se>
2013-12-28 15:10:42 +0100

    now able to parse multiple tickers

--- ValueCalc.hs
+++ ValueCalc.hs
@@ -2,24 +2,26 @@ module ValueCalc (
    main
 ) where
 
-import KParser
 import Scraper
+import Control.Monad
 
-{-
-   use the loadMDCA function get data points
-   from the edgar website
-
-   TODO: read the data properly and pass 
-         the information to some calculation module
--}
 main :: IO ()
 main =
-   do loadMDCA
-      putStrLn "exiting"
+   do loadCompanyList ["mdca","msft"]
+      putStrLn "----exiting----"
 
-loadMDCA :: IO ()
-loadMDCA =
-   do info <- parse "http://stockreports.nasdaq.edgar-online.com/msft.html"
+edgarLink :: String
+edgarLink = "http://stockreports.nasdaq.edgar-online.com/"
+
+printCompany :: IO Company -> IO ()
+printCompany comp =
+   do info <- comp
+      putStrLn "_____________"
       putStrLn $ "name: " ++ (name info)
       putStrLn $ "total assets: " ++ show (totalAssets info)
       putStrLn $ "total liabilities: " ++ show (totalLiabilities info)
+
+loadCompanyList :: [String] -> IO ()
+loadCompanyList ls =
+   do let ps = [parse (edgarLink++x++".html")|x<-ls]
+      forM_ ps printCompany
